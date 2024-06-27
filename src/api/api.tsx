e2e3f95c1440c9f27a5ac2c3435f402c09df1c07
Pe-te-r <mburupeter8526@gmail.com>
2024-06-27 17:43:12 +0300

    full api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api', 
+    baseURL: 'https://book-api-1-8wmi.onrender.com/api/', 
     // timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
